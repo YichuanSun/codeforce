34b7f94166b32183e6030481f5421da3811eb57b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-05-23 00:05:40 +0800

    .

--- 2018.5.21/A.cpp
+++ 2018.5.21/A.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cmp(const void *a,const void *b)	{return *(int*)a-*(int*)b;}
 int main()	{
 	int n,a[105]={0};
 	scanf("%d",&n);
 	int j=n/2,sumb=0,sumw=0;
 	for (int i=1;i<=j;i++)	scanf("%d",&a[i]);
-	qsort(a,n/2,sizeof(int),cmp);
+	sort(a+1,a+j+1);
 	for (int i=1;i<=j;i++)	{
 		sumb+=abs(2*i-1-a[i]);
 		sumw+=abs(2*i-a[i]);
