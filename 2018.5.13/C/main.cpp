8034705944032b197cee1a89545547f3bd0fb544
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-06-07 10:32:38 +0800

    fuck'

---
+++ 2018.5.13/C/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define SIZE 200010
+using namespace std;
+typedef long long ll;
+int sear(ll env,ll group[]);
+ll group[SIZE],temp,env;
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    int n,m;
+    cin >> n >> m;
+    for (int i=1;i<=n;i++)   {
+        cin>>temp;
+        group[i]=group[i-1]+temp;
+    }
+    //for (int i=0;i<=n;i++)  cout << i << " " << group[i] << endl;
+    for (int i=0;i<m;i++)   {
+        cin>>env;
+        int num=sear(env,group);
+        cout<<num<<" "<<env-group[num-1]<<endl;
+    }
+    return 0;
+}
+
+int sear(ll env,ll group[])  {
+    int i;
+    for (i=1;env>group[i];i++);
+    return i;
+}
