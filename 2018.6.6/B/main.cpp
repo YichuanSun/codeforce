dd4dd86fbe2f634cf18f8ab57d72a281cc10737f
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-06-10 16:52:14 +0800

    .

--- 2018.6.6/B/main.cpp
+++ 2018.6.6/B/main.cpp
@@ -1,26 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 map<string,int> map_str;
-bool cmp(pair<string,int> &a,pair<string,int> &b)   {
+bool cmp(pair<string,int> a,pair<string,int> b)   {
     if (a.second>b.second)    return true;
     else return false;
 }
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0),cout.tie(0);
-    int n;
+    int n,mmax=-1;
     cin>>n;
     string s;
     cin>>s;
     for (int i=0;i<n-1;i++)   {
         string stemp;
-        stemp=s.substr(i,2);
+        stemp=s.substr(i,2);        //666
         map_str[stemp]++;
     }
-    map<string,int>::iterator iter;
-    for (iter=map_str.begin();iter!=map_str.end();iter++)   {
-        cout<<iter->first<<'\t'<<iter->second<<endl;
+    vector<pair<string,int>> vmap;
+    map<string,int>::iterator iter;     //666
+    for (iter=map_str.begin();iter!=map_str.end();iter++)   {   //666
+        //cout<<iter->first<<'\t'<<iter->second<<endl;
+        vmap.push_back(make_pair(iter->first,iter->second));
     }
-    sort(map_str.begin(),map_str.end(),cmp);
+    sort(vmap.begin(),vmap.end(),cmp);
+    string ans;
+    for (auto x:vmap)   {
+        if (x.second>mmax)  {
+                ans=x.first;
+                mmax=x.second;
+        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
