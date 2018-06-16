cff212efed615191b22fd6ba1d1d1ab23f635611
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-06-16 22:32:00 +0800

    .

---
+++ 2018.6.11/A/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    string s;
+    cin>>s;
+    if (s.size()<3)   {
+        cout<<"No\n";
+        return 0;
+    }
+    for (int i=0;i<s.size()-2;i++)   {
+        if (s[i]+s[i+1]+s[i+2]=='A'+'B'+'C'&&s[i]!=s[i+2])    {
+            cout<<s[i]<<s[i+1]<<s[i+2]<<endl;
+            cout<<"Yes\n";
+            return 0;
+        }
+    }
+    cout<<"No\n";
+    return 0;
+}
