c6f7c7706fe1c94d2bdde860172caf378850de1a
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-03 14:56:13 +0800

    自己的算法就是渣渣

--- 2018.9.2/B/main.cpp
+++ 2018.9.2/B/main.cpp
@@ -1,35 +1,20 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n, s;
-    cin >> n >> s;
+int main()  {
+    int n,s;
+    long long ans=0;
+    cin>>n>>s;
     vector<int> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
+    for (int i=0;i<n;i++){
+        cin>>a[i];
     }
-    sort(a.begin(), a.end());
-    long long ans = 0;
-    for (int i = 0; i <= n / 2; i++)
-    {
-        if (a[i] > s)
-        {
-            ans += a[i] - s;
-        }
-    }
-    for (int i = n / 2; i < n; i++)
-    {
-        if (a[i] < s)
-        {
-            ans += s - a[i];
-        }
-    }
-    cout << ans << '\n';
+    sort(a.begin(),a.end());
+    for (int i=0;i<=n/2;i++)
+        if (a[i]>s) ans+=a[i]-s;
+    for (int i=n/2;i<n;i++)
+        if (a[i]<s) ans+=s-a[i];
+    cout<<ans<<endl;
     return 0;
 }
 
