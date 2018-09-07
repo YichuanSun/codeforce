b4a040d1f9854fce1091b117ed014cc6b40ee253
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-07 21:22:23 +0800

    原来是题意没理解好，真是。。。

--- 2018.9.6/A/main.cpp
+++ 2018.9.6/A/main.cpp
@@ -1,33 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<char,int> p;
-map<char,int> mp;
-vector<p> v;
-bool cmp(p a,p b)   {
-    return a.second>b.second;
-}
+int times[30];
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,k,cnt=0;
+    int n,k,ans=10000000;
     string s;
     cin>>n>>k;
     cin>>s;
-    for (int i=0;i<s.size();i++)    {
-        if (mp[s[i]]==0)   cnt++;
-        mp[s[i]]++;
-    }
-    if (cnt<k)  cout<<0<<endl;
-    else    {
-        for (map<char,int>::iterator j=mp.begin();j!=mp.end();j++)
-            v.push_back(*j);
-        sort(v.begin(),v.end(),cmp);
-        int sum=0,mi=1000000;
-        for (int i=0;i<k;i++)   {
-            sum+=v[i].second;
-            if (v[i].second<mi) mi=v[i].second;
-        }
-        cout<<mi*k+s.size()-sum<<endl;
-    }
+    for (int i=0;i<n;i++)   times[s[i]-'A']++;
+    for (int i=0;i<k;i++)   ans=min(ans,times[i]);
+    cout<<ans*k<<endl;
     return 0;
 }
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//typedef pair<char,int> p;
+//map<char,int> mp;
+//vector<p> v;
+//bool cmp(p a,p b)   {
+//    return a.second>b.second;
+//}
+//int main()  {
+//    ios::sync_with_stdio(false);
+//    cin.tie(0);
+//    int n,k,cnt=0;
+//    string s;
+//    cin>>n>>k;
+//    cin>>s;
+//    for (int i=0;i<s.size();i++)    {
+//        if (mp[s[i]]==0)   cnt++;
+//        mp[s[i]]++;
+//    }
+//    if (cnt<k)  cout<<0<<endl;
+//    else    {
+//        for (map<char,int>::iterator j=mp.begin();j!=mp.end();j++)
+//            v.push_back(*j);
+//        sort(v.begin(),v.end(),cmp);
+//        int sum=0,mi=1000000;
+//        for (int i=0;i<k;i++)   {
+//            sum+=v[i].second;
+//            if (v[i].second<mi) mi=v[i].second;
+//        }
+//        cout<<mi*k+s.size()-sum<<endl;
+//    }
+//    return 0;
+//}
