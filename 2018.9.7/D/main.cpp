f3c89eda1e7837f3ed05576e7b28cf58706e82b1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-10 17:32:38 +0800

    .

--- 2018.9.7/D/main.cpp
+++ 2018.9.7/D/main.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 #define N 100005
 using namespace std;
+typedef long long ll;
 int a[N],b[N];
 int main()  {
-    int n,m,suma=0,sumb=0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m;
+    ll suma=0,sumb=0;
     cin>>n;
     for (int i=0;i<n;i++)   {
         cin>>a[i];
@@ -19,9 +23,9 @@ int main()  {
         return 0;
     }
     int posa=0,posb=0,ans=0;
-    while (posa<n&&posb<n)  {
+    while (posa<n&&posb<m)  {
         ans++;
-        int sa=a[posa++],sb=b[posb++];
+        ll sa=a[posa++],sb=b[posb++];
         while (sa!=sb)  {
             if (sa<sb)  sa+=a[posa++];
             else    sb+=b[posb++];
