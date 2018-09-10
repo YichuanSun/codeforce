eaf79ad98ec4f2db2bb0524e15942a537be1320d
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-10 14:58:28 +0800

    深搜真灵活

--- 2018.9.7/C/main.cpp
+++ 2018.9.7/C/main.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+vector<ll> v;
+void brute(int lev,ll cu,int cnt)    {
+    if (lev==18)    {
+        v.push_back(cu);
+        return;
+    }
+    brute(lev+1,cu*10,cnt);
+    if (cnt<3)  {
+        for (int i=1;i<=9;i++)
+            brute(lev+1,cu*10+i,cnt+1);
+    }
+}
 
 int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    brute(0,0,0);
+    v.push_back(1e18);
+    ll l,r;
     int n;
     cin>>n;
-    long long a,b;
     for (int i=0;i<n;i++)   {
-        cin>>a>>b;
-        int tans=0;
-        for (int i=a;i<=b;i++)  {
-            int t=i,cnt=0,flag=1;
-            while (t!=0)    {
-                if (t%10)   cnt++;
-                t/=10;
-                if (cnt>3)   {
-                    flag=0;
-                    break;
-                }
-            }
-            if (flag)  tans++;
-        }
-        cout<<tans<<endl;
+        cin>>l>>r;
+        int ans=upper_bound(v.begin(),v.end(),r)-lower_bound(v.begin(),v.end(),l);
+        cout<<ans<<endl;
     }
     return 0;
 }
