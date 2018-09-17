9766159f3dca3ed708e6be95ccd76c73d246e786
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-17 16:27:44 +0800

    .

---
+++ 2018.9.17/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  {
+    int n,m,t,sum=0,mx=-1,ansmi=0,ansmx=0;
+    scanf("%d%d",&n,&m);
+    for (int i=0;i<n;i++) {
+        scanf("%d",&t);
+        sum+=t;
+        if (t>mx)   mx=t;
+    }
+    int tot=m+sum;
+    if (tot%n==0)   ansmi=tot/n;
+    else    ansmi=tot/n+1;
+    ansmx=mx+m;
+    printf("%d %d\n",ansmi,ansmx);
+    return 0;
+}
