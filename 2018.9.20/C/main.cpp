2fe71b19e794473c928ad6b4d729c8cefe2da020
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-21 14:13:15 +0800

    丧心病狂的用例数，113个。
    昨晚上不对原来是因为题目理解错了。。。

--- 2018.9.20/C/main.cpp
+++ 2018.9.20/C/main.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
+#define N 105
 using namespace std;
+int s[N],des[N];
+char ans[N];
 map<int,int> mp;
-vector<int> v;
+vector<int> three,one;
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,t,des[105];
-    char st[105];
-    fill(st,st+105,'B');
-    fill(des,des+105,-1);
+    fill(ans,ans+N,'B');
+    int n;
     cin>>n;
     for (int i=0;i<n;i++)   {
-        cin>>t;
-        mp[t]++;
-        des[t]=i;
+        cin>>s[i];
+        mp[s[i]]++;
+        des[s[i]]=i;
     }
     map<int,int>::iterator it1=mp.begin();
-    for (;it1!=mp.end();it1++)
+    for (;it1!=mp.end();it1++)  {
         if ((*it1).second==1)
-            v.push_back((*it1).first);
-    if ((int)v.size()&1)    {
+            one.push_back((*it1).first);
+        else if ((*it1).second>=3)
+            three.push_back((*it1).first);
+    }
+    if (((int)one.size()&1)&&three.empty())
         cout<<"NO"<<endl;
-        return 0;
-    }
-    cout<<"YES"<<endl;
-    if ((int)v.size())  {
-        for (int i=0;i<(int)v.size()/2;i++)
-            st[des[v[i]]]='A';
-        for (int i=0;i<n;i++)   cout<<st[i];
-        cout<<endl;
-    }
     else {
-        for (int i=0;i<n/2;i++)   st[i]='A';
-        for (int i=0;i<n;i++)   cout<<st[i];
-        cout<<endl;
+        cout<<"YES"<<endl;
+        if (!((int)one.size()&1))    {
+            for (int i=0;i<(int)one.size()/2;i++) ans[des[one[i]]]='A';
+            for (int i=0;i<n;i++)   cout<<ans[i];
+            cout<<endl;
+        }
+        else if (((int)one.size()&1)&&!three.empty())  {
+            for (int i=0;i<(int)one.size()/2;i++) ans[des[one[i]]]='A';
+            ans[des[three[0]]]='A';
+            for (int i=0;i<n;i++)   cout<<ans[i];
+            cout<<endl;
+        }
     }
     return 0;
 }
