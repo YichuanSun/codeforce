dc3290261c8f3b5cbf2e59088d365ddcad5a4caf
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-06 23:19:15 +0800

    好

--- 2018.10.4/C/main.cpp
+++ 2018.10.4/C/main.cpp
@@ -21,27 +21,27 @@ int main()  {
             ma[len]=min(ma[len],tsm);
         }
     }
-    for (int i=1;i<=m;i++)  {
-        ll c=x/ma[i];
-        int p1=1,p2=n,f=0;
-        while (p1<p2)   {       //用双指针法，则无法遍历所有的可能的区间值，就是说b[p2]-b[p1]不会包括所有的部分和值，也就错了
-            if (c<b[p2]-b[p1])  p1++;
-            else if (c>b[p2]-b[p1]) p2--;
-            else {f=1;break;}
-        }
-        if (f==0)   continue;
-        int len=p2-p1+1;
-        cout<<"len: "<<len<<" i:"<<i<<endl;
-        ans=max(ans,len*i);
-    }
-//    for (int i=1;i<=m;i++)  {
-//        for (int j=i;j<=m;j++)  {
-//            ll sc=x/(b[j]-b[i-1]);
-//            int len=j-i+1;
-//            int mxa=upper_bound(ma+1,ma+n+1,sc)-ma-1;
-//            ans=max(ans,len*mxa);
+//    for (int i=1;i<=m;i++)  { //此法错误
+//        ll c=x/ma[i];
+//        int p1=1,p2=n,f=0;
+//        while (p1<p2)   {       //用双指针法，则无法遍历所有的可能的区间值，就是说b[p2]-b[p1]不会包括所有的部分和值，也就错了
+//            if (c<b[p2]-b[p1])  p1++;
+//            else if (c>b[p2]-b[p1]) p2--;
+//            else {f=1;break;}
 //        }
+//        if (f==0)   continue;
+//        int len=p2-p1+1;
+//        cout<<"len: "<<len<<" i:"<<i<<endl;
+//        ans=max(ans,len*i);
 //    }
+    for (int i=1;i<=m;i++)  {
+        for (int j=i;j<=m;j++)  {
+            ll sc=x/(b[j]-b[i-1]);
+            int len=j-i+1;
+            int mxa=upper_bound(ma+1,ma+n+1,sc)-ma-1;
+            ans=max(ans,len*mxa);
+        }
+    }
     cout<<ans<<endl;
     return 0;
 }
