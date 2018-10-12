1e307dd16a9470e5df5148f010a34d22cf880bcd
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-12 23:09:16 +0800

    今天鬼课有点多，而且两天没跑步，有点疲了，在走下坡路，小心

---
+++ 2018.10.12/A/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define N 1000000005
+using namespace std;
+int main()  {
+    int t,L,v,l,r;
+    cin>>t;
+    while (t--) {
+        cin>>L>>v>>l>>r;
+
+    }
+    return 0;
+}
