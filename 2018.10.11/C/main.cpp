6385efa51ffed3df2790cd65b7f101493a40465b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-13 12:10:04 +0800

    ？

--- 2018.10.11/C/main.cpp
+++ 2018.10.11/C/main.cpp
@@ -1,22 +1,45 @@
-#include <bits/stdc++.h>
-#define N 200005
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#define LL long long
 using namespace std;
-typedef long long ll;
-ll ans,sum,tt;
-int a[N];
-int main()  {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n,k,mn=1000000;
+LL n,k;
+int a[200050];
+bool cmp(int x,int y){
+    return x>y;
+}
+int minn=2147483647,maxn=0;
+int b[200050];
+int main(){
     cin>>n>>k;
-    for (int i=0;i<n;i++)   {
-        cin>>a[i];
-        mn=min(mn,a[i]);
-        sum+=a[i];
+    for(int i(1);i<=n;i++)scanf("%d",&a[i]),minn=min(minn,a[i]),maxn=max(maxn,a[i]);
+    if(n==1){
+        cout<<0;
+        return 0;
     }
-    tt=sum-mn*n;
-    if (tt%k==0)    ans=tt/k;
-    else ans=tt/k+1;
-    cout<<ans<<endl;
+    for(int i(1);i<=n;i++){
+        a[i]-=minn;
+        b[1]++;
+        b[a[i]+1]--;
+    }
+    for (int i=1;i<=maxn;i++)   cout<<b[i]<<' ';
+    cout<<endl;
+    for(int i(2);i<=n;i++){
+        if(a[i]!=a[1])break;
+        if(i==n){
+            cout<<0;
+            return 0;
+        }
+    }
+    for(int i(2);i<=maxn;i++)b[i]+=b[i-1];
+    int tmp=0,ans=1;
+    for (int i=1;i<=maxn;i++)   cout<<b[i]<<' ';
+    cout<<endl;
+    for(int i(1);i<=maxn;i++){
+        if(tmp+b[i]<=k)tmp+=b[i];
+        else ans++,tmp=b[i];
+    }
+    printf("%d",ans);
     return 0;
 }
