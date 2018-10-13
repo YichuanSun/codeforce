099382312f2cc3fb09d711d27150099b9b183ee6
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-13 23:33:02 +0800

    可能是这两天不知道做哪个方向了吧，状态有点下降

--- 2018.10.12/B/main.cpp
+++ 2018.10.12/B/main.cpp
@@ -9,18 +9,17 @@ int main()  {
     for (int i=0;i<n;i++)   cin>>e[i];
     int i,ans=0,len=0;
     for (i=0;i<n;i++)   {
-        for (int j=i==0?i+r-1:i+r+1;j>=i;j--)    {
+        for (int j=i==0?i+r-1:i+r+r-1;j>=i;j--)    {
             if (j>=n)   continue;
             if (e[j]==1)    {
                 len+=j-i+1;
                 ans++;
                 i=j;
-                cout<<len<<endl;
                 break;
             }
         }
     }
     cout<<len<<endl;
-    cout<<"ans:"<<ans+1<<endl;
+    cout<<"ans:"<<ans<<endl;
     return 0;
 }
