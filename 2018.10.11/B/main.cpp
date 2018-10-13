68eadcbdcca63f0428ac6279ca3e101ff68c9805
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-13 09:38:19 +0800

    这可真是。。。水题一道，但是读错题了，以为是无环图，没想到只是个无自环图

--- 2018.10.11/B/main.cpp
+++ 2018.10.11/B/main.cpp
@@ -4,18 +4,19 @@ typedef long long ll;
 ll n,m;
 int main()  {
     cin>>n>>m;
-    ll mmx=n-(m+1);
-    ll mmn;
+    ll mmn,mmx;
     if (m==0)   {
         cout<<n<<' '<<n<<endl;
         return 0;
     }
-    if (m>=n-1) {
-        cout<<0<<' '<<0<<endl;
-        return 0;
-    }
     if (m>n/2)  mmn=0;
     else mmn=n-m*2;
+    for (ll k=0;;k++)  {
+        if ((k-1)*k/2>=m)    {
+            mmx=n-k;
+            break;
+        }
+    }
     cout<<mmn<<' '<<mmx<<endl;
     return 0;
 }
