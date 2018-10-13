d13d1ec2e3377cbb88f8693dc593784991721b3a
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-13 18:07:19 +0800

    又是计算下标，还是比较懵

---
+++ 2018.10.12/B/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define N 1005
+using namespace std;
+
+int e[N],n,r;
+
+int main()  {
+    cin>>n>>r;
+    for (int i=0;i<n;i++)   cin>>e[i];
+    int i,ans=0,len=0;
+    for (i=0;i<n;i++)   {
+        for (int j=i==0?i+r-1:i+r+1;j>=i;j--)    {
+            if (j>=n)   continue;
+            if (e[j]==1)    {
+                len+=j-i+1;
+                ans++;
+                i=j;
+                cout<<len<<endl;
+                break;
+            }
+        }
+    }
+    cout<<len<<endl;
+    cout<<"ans:"<<ans+1<<endl;
+    return 0;
+}
