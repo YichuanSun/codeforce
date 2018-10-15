0ac15a0677073f8ba3c2624925777e2a4a9d3b1a
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-15 13:39:09 +0800

    打表找规律，可怜我当初表也打错了，没打二进制表

--- 2018.10.14/B/main.cpp
+++ 2018.10.14/B/main.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
+#define N 10005
 using namespace std;
 typedef long long ll;
 int main(){
     int t;
     cin>>t;
     for (int i=0;i<t;i++)   {
-        ll a;
+        int bi[N];
+        ll a,cnt=0,ct=0;
         cin>>a;
+        ll ta=a;
+        while (ta)  {
+            bi[cnt++]=(ta&1);
+            ta>>=1;
+        }
+        reverse(bi,bi+cnt);
+        for (int i=0;i<cnt;i++) cout<<bi[i];
+        cout<<'\t';
         for (int j=0;j<=a;j++)
-            cout<<((a-j)^a)<<'\t'<<j<<endl;
+            if (a-(a^j)==j) ct++;
+        cout<<ct<<endl;
     }
     return 0;
 }
