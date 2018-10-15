32b5d135a3b89531c562c880225b9b97dbff07b8
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-15 14:48:26 +0800

    这么一看，确实水死了。但是打表找规律，一定要记得这类提的模样

--- 2018.10.14/B/main.cpp
+++ 2018.10.14/B/main.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
-#define N 10005
 using namespace std;
-typedef long long ll;
-int main(){
-    int t;
-    cin>>t;
-    for (int i=0;i<t;i++)   {
-        int bi[N];
-        ll a,cnt=0,ct=0;
+
+int main()  {
+    int n,a;
+    cin>>n;
+    for (int i=0;i<n;i++)   {
+        int ans=1;
         cin>>a;
-        ll ta=a;
-        while (ta)  {
-            bi[cnt++]=(ta&1);
-            ta>>=1;
+        if (!a) {cout<<1<<endl;continue;}
+        while (a)   {
+            if (a&1)    ans<<=1;
+            a>>=1;
         }
-        reverse(bi,bi+cnt);
-        for (int i=0;i<cnt;i++) cout<<bi[i];
-        cout<<'\t';
-        for (int j=0;j<=a;j++)
-            if (a-(a^j)==j) ct++;
-        cout<<ct<<endl;
+        cout<<ans<<endl;
     }
     return 0;
 }
