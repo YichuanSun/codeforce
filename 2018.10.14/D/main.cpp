32c9ef70158b42327ff3843101a056771fe9e53b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-15 15:59:29 +0800

    dfs的一些思考

--- 2018.10.14/D/main.cpp
+++ 2018.10.14/D/main.cpp
@@ -1,56 +1,35 @@
 #include <bits/stdc++.h>
 #define N 2005
 using namespace std;
-char graph[N][N];
+char g[N][N];
 bool visit[N][N];
-int n,m,r,c,x,y,ans,cnt=0;
-void dfs(int x,int y,int rt,int lt);
+struct node{
+    int r,c,x,y;
+    node(int rr,int cc,int xx,int yy)
+        {x=xx,y=yy,r=rr,c=cc;}
+};
+deque<node> dq;
 int main()  {
+    int n,m,r,c,x,y,ans=0;
     cin>>n>>m>>r>>c>>x>>y;
     for (int i=1;i<=n;i++)
         for (int j=1;j<=m;j++)
-            cin>>graph[i][j];       //此处图没问题
-    dfs(r,c,x,y);
-    cout<<endl;
-    for (int i=1;i<=n;i++)  {
-        for (int j=1;j<=m;j++)  {
-            if (visit[i][j])    cout<<'+';
-            else cout<<graph[i][j];
-        }
-        cout<<endl;
+            cin>>g[i][j];
+    dq.push_back(node(r,c,x,y));
+    while (!dq.empty()) {
+        node tn=dq.front();dq.pop_front();
+        int nx=tn.r,ny=tn.c;
+        if (visit[nx][ny])  continue;
+        visit[nx][ny]=true;ans++;
+//        if (ny-1>0&&g[nx][ny-1]=='.'&&tn.x)    dq.push_front(node(nx,ny-1,tn.x-1,tn.y));
+//        if (ny+1<=m&&g[nx][ny+1]=='.'&&tn.y)   dq.push_front(node(nx,ny+1,tn.x,tn.y-1));
+//        if (nx-1>0&&g[nx-1][ny]=='.')   dq.push_front(node(nx-1,ny,tn.x,tn.y));
+//        if (nx+1<=n&&g[nx+1][ny]=='.')   dq.push_front(node(nx+1,ny,tn.x,tn.y));
+        if (nx-1>0&&g[nx-1][ny]=='.')   dq.push_front(node(nx-1,ny,tn.x,tn.y));
+        if (nx+1<=n&&g[nx+1][ny]=='.')   dq.push_front(node(nx+1,ny,tn.x,tn.y));
+        if (ny-1>0&&g[nx][ny-1]=='.'&&tn.x)    dq.push_back(node(nx,ny-1,tn.x-1,tn.y));
+        if (ny+1<=m&&g[nx][ny+1]=='.'&&tn.y)   dq.push_back(node(nx,ny+1,tn.x,tn.y-1));
     }
     cout<<ans<<endl;
     return 0;
-}
-
-void dfs(int x,int y,int lt,int rt)  {
-    if (x<=0||x>n||y<=0||y>n)   return;
-    if (visit[x][y]||graph[x][y]=='*')    return;
-    //if (x!=r&&y!=c)
-    cout<<"Case:"<<cnt++<<endl;
-    cout<<endl;
-    for (int i=1;i<=n;i++)  {
-        for (int j=1;j<=m;j++)  {
-            if (visit[i][j])    cout<<'+';
-            else cout<<graph[i][j];
-        }
-        cout<<endl;
-    }
-        visit[x][y]=true;
-    ans++;
-    for (int dx=-1;dx<=1;dx++)  {
-        if (lt) {
-            dfs(x+dx,y-1,lt-1,rt);
-//            if (!(x+dx<=0||x+dx>n||y-1<=0||y-1>n))
-//                visit[x+dx][y-1]=false;
-        }
-        dfs(x+dx,y,lt,rt);
-//        if (!(x+dx<=0||x+dx>n||y<=0||y>n))
-//            visit[x+dx][y]=false;
-        if (rt) {
-            dfs(x+dx,y+1,lt,rt-1);
-//            if (!(x+dx<=0||x+dx>n||y+1<=0||y+1>n))
-//                visit[x+dx][y+1]=false;
-        }
-    }
 }
