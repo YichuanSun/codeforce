1f1c04f202d7b75c91de5ae91b340344b2652d42
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-21 21:32:50 +0800

    这么水的题，我当初居然做错了。。。

--- 2018.10.21/C/main.cpp
+++ 2018.10.21/C/main.cpp
@@ -1,53 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> vmi,vmx;
-set<int> st;
+vector<int> va,vb;
 int main()  {
     int a,b;
     cin>>a>>b;
-    int mx=max(a,b),mi=min(a,b);
-    int tp=sqrt(2.0*(a+b));
-    if (tp<mi)  {
-        mi-=tp;
-        st.insert(tp);
-        vmi.push_back(tp);
+    int n=sqrt(2.0*(a+b));
+    for (int i=n;i>=1;i--)  {
+        if (a>=i)   {a-=i;va.push_back(i);}
+        else    vb.push_back(i);
     }
-    for (int i=1;i<=tp;i++) {
-        if (st.count(i))    continue;
-        if (i<=mi)  {
-            st.insert(i);
-            vmi.push_back(i);
-            mi-=i;
-        }
-        else if (i<=mx) {
-            st.insert(i);
-            vmx.push_back(i);
-            mx-=i;
-        }
-    }
-    if (mx==a)  {
-        cout<<vmx.size()<<endl;
-        if (!vmx.empty())   cout<<vmx[0];
-        for (int i=1;i<(int)vmx.size();i++)
-            cout<<' '<<vmx[i];
-        cout<<endl;
-        cout<<vmi.size()<<endl;
-        if (!vmi.empty())   cout<<vmi[0];
-        for (int i=1;i<(int)vmi.size();i++)
-            cout<<' '<<vmi[i];
-        cout<<endl;
-    }
-    else {
-        cout<<vmi.size()<<endl;
-        if (!vmi.empty())   cout<<vmi[0];
-        for (int i=1;i<(int)vmi.size();i++)
-            cout<<' '<<vmi[i];
-        cout<<endl;
-        cout<<vmx.size()<<endl;
-        if (!vmx.empty())   cout<<vmx[0];
-        for (int i=1;i<(int)vmx.size();i++)
-            cout<<' '<<vmx[i];
-        cout<<endl;
-    }
+    cout<<va.size()<<endl;
+    if (!va.empty())    cout<<va[0];
+    for (int i=1;i<(int)va.size();i++)
+        cout<<' '<<va[i];
+    cout<<endl;
+    cout<<vb.size()<<endl;
+    if (!vb.empty())    cout<<vb[0];
+    for (int i=1;i<(int)vb.size();i++)
+        cout<<' '<<vb[i];
+    cout<<endl;
     return 0;
 }
