bd7ba976891db9effbce6ca924edb3c0781bd876
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-11-12 14:57:16 +0800

    我抱起那个落满了尘土的github

--- 2018.10.24/A/main.cpp
+++ 2018.10.24/A/main.cpp
@@ -1,18 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main()  {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n,m,k,l,x;
-    cin>>n>>m>>k>>l;
-    int a=l/m,b=(n-k)/m;
-    for (x=a;x<=b;x++)  {
-        if (x*m>=l&&x*m<n-k)    {
-            break;
-        }
+int main()
+{
+    int64_t n, m, k, l;
+    cin >> n >> m >> k >> l;
+    if (n - k < l || (k+l + m-1) / m * m > n) {
+        cout << -1 << endl;
+    } else {
+        cout << (k+l + m-1) / m << endl;
     }
-    if (x==b+1) cout<<-1<<endl;
-    else    cout<<x<<endl;
-    return 0;
 }
+
+//第一题难以理解
