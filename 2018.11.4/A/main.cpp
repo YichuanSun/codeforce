a0b9502e51c53ee1f07699e7aa66365d4bf0324b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-11-12 20:35:39 +0800

    水

---
+++ 2018.11.4/A/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()  {
+    ll n,x,y;
+    cin>>n>>x>>y;
+    if (x+y>n+1)    cout<<"Black"<<endl;
+    else    cout<<"White"<<endl;
+    return 0;
+}
