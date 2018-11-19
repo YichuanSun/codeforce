2a9220ef1f892e62430b1b9d6235ae6dfadfcfe4
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-11-19 19:22:16 +0800

    水平是真的下降了

--- 2018.11.16/C/main.cpp
+++ 2018.11.16/C/main.cpp
@@ -2,27 +2,24 @@
 #define N 200005
 using namespace std;
 typedef long long ll;
-ll a[N];
-vector<int> vi;
+int a[N];
+ll s;
+map<ll,int> mi;
+vector<int> ans;
 int main()  {
-    ll n,sum=0;
+    int n;
     cin>>n;
+    for (int i=0;i<n;i++)   {cin>>a[i];s+=a[i];mi[a[i]]++;}
     for (int i=0;i<n;i++)   {
-        cin>>a[i];
-        sum+=a[i];
+        ll ts=s-a[i];
+        mi[a[i]]--;
+        if ((ts&1)==0&&mi[ts/2])    ans.push_back(i+1);
+        mi[a[i]]++;
     }
-    sort(a,a+n);
-    cout<<sum<<endl;
-    for (int i=0;i<n;i++)   {
-        ll nw=sum-a[i];
-        ll* lb=lower_bound(a,a+n,nw);
-        cout<<i<<' '<<*(lb)<<endl;
-        if (*(lb)==nw)
-            vi.push_back(i+1);
-    }
-    if (vi.size())  cout<<vi[0];
-    for (int i=1;i<(int)vi.size();i++)
-        cout<<' '<<vi[i];
+    cout<<ans.size()<<'\n';
+    if (!ans.empty())   cout<<ans[0];
+    for (int i=1;i<(int)ans.size();i++)
+        cout<<' '<<ans[i];
     cout<<endl;
     return 0;
 }
