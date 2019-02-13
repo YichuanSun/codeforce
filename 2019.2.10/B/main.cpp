574d874d05fdb44b427087da6770f1dce07f1803
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-02-13 22:23:13 +0800

    我应该能想到的啊，为什么当时没想到？难道是因为当时在嗑瓜子吗？

--- 2019.2.10/B/main.cpp
+++ 2019.2.10/B/main.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
-#define N 5005
+#define N 200005
 using namespace std;
-
+typedef pair<int,int> pii;
+vector<pii> a(N);
+bool cmp(pii a,pii b)   {
+    return a.first>b.first;
+}
+int des[N],mx;
 int main()  {
-    int n;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,m,k;
+    cin>>n>>m>>k;
+    for (int i=0;i<n;i++)   {
+        cin>>a[i].first;
+        a[i].second=i;
+    }
+    sort(a.begin(),a.end(),cmp);
+    for (int i=0;i<m*k;i++){
+        mx+=a[i].first;
+        des[i]=a[i].second;
+    }
     return 0;
 }
