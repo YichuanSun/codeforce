a1d5071e0a2caaa135a7f7139592d2dbc52d54fd
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-02-23 19:41:27 +0800

    ？？？

--- 2019.2.23/B/main.cpp
+++ 2019.2.23/B/main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define N 10005
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
+typedef pair<int,int> pii;
+typedef long long ll;
+ll ans=1;
+pii ps[N];
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,im=0;
+    cin>>n;
+    for (int i=0;i<n;im++,i++)   {
+        cin>>ps[im].first>>ps[im].second;
+        if (ps[im].first==ps[im].second)  im--;
+    }
+    if (im==0||im==1)   {
+        cout<<1+min(ps[0].first,ps[0].second)<<endl;
+        return 0;
+    }
+    for (int i=1;i<n;i++)   {
+        int prea=ps[i-1].first,preb=ps[i-1].second;
+        int a=ps[i].first,b=ps[i].second;
+        int mx=max(prea,preb),mn=min(a,b);
+        //cout<<mx<<' '<<mn<<endl;
+        if (mn>=mx) ans+=mn-mx+1;
+//        if (a==b)   {
+//            if (a>0)    ans+=a-max(prea,preb);
+//            else continue;
+//        }
+//        if (a>b)   ans+=b-prea;
+//        else if (a<b)  {
+//            ans+=a-max(prea,preb);
+//            ans+=(prea==preb);
+//        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
