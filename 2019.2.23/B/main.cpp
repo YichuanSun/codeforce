3206629aaf4af287e105d801038a9cdcdada93ea
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-02-25 21:31:19 +0800

    好短的代码，可我就是没写出来。应该考虑成区间，统一端点选取与否

--- 2019.2.23/B/main.cpp
+++ 2019.2.23/B/main.cpp
@@ -1,42 +1,16 @@
 #include <bits/stdc++.h>
-#define N 10005
 using namespace std;
-typedef pair<int,int> pii;
-typedef long long ll;
-ll ans=1;
-pii ps[N];
+
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,im=0;
+    int n,a0=0,b0=0,ans=1,a,b;
     cin>>n;
-    for (int i=0;i<n;im++,i++)   {
-        cin>>ps[im].first>>ps[im].second;
-        if ((i!=n-1)&&ps[im].first==ps[im].second)  im--;
-    }
-    cout<<' '<<n<<endl;
-    if (im==0||im==1)   {
-        cout<<1+min(ps[0].first,ps[0].second)<<endl;
-        return 0;
-    }
-
-    for (int i=0;i<n;i++)
-        cout<<"i="<<i<<' '<<ps[i].first<<' '<<ps[i].second<<endl;
-    for (int i=1;i<n;i++)   {
-        int prea=ps[i-1].first,preb=ps[i-1].second;
-        int a=ps[i].first,b=ps[i].second;
-        if (a>b)    {
-            if (b>=prea)    {
-                if (b>preb) ans+=a-b+1;
-                else    ans+=a-b;
-            }
-        }
-        else    {
-            if (a>=preb)    {
-                if (a>prea) ans+=b-a+1;
-                else    ans+=b-a;
-            }
-        }
+    for (int i=1;i<=n;i++)   {
+        cin>>a>>b;
+        ans+=max(0,min(a,b)-max(a0,b0)+1);
+        if (a0==b0) ans--;
+        a0=a,b0=b;
     }
     cout<<ans<<endl;
     return 0;
