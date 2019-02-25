3b33075754b7f939e77d90092b6b67190a0bfc4e
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-02-25 20:47:03 +0800

    .

--- 2019.2.19/B/main.cpp
+++ 2019.2.19/B/main.cpp
@@ -1,38 +1,27 @@
 #include <bits/stdc++.h>
 #define N 200005
 using namespace std;
-typedef long long ll;
-ll nowoddsum,nowevensum,sum[2][N];
-int a[N];
+int  a[N],ltor[N],rtol[N],n,ans;
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n,ans=0;
     cin>>n;
-    for (int i=1;i<=n;i++)   {
-        cin>>a[i];
-        if ((i&1)==0) {
-            if (i>2)
-                sum[0][i]+=sum[0][i-2]+a[i];
-            else
-                sum[0][i]+=a[i];
-            sum[1][i]=sum[0][i];
+    for (int i=0;i<n;i++)   cin>>a[i];
+    for (int i=0;i<n;i++)
+        ltor[i]=a[i]+(i>=2?ltor[i-2]:0);
+    for (int i=n-1;i>=0;i--)
+        rtol[i]=a[i]+rtol[i+2];
+    for (int i=0;i<n;i++)   {
+        int a=0,b=0;
+        if (i>=2)   {
+            a=ltor[i-2];
+            b=ltor[i-1];
         }
-        else {
-            if (i>1)
-                sum[1][i]+=sum[1][i-2]+a[i];
-            else
-                sum[1][i]+=a[i];
-            sum[0][i]=sum[1][i];
-        }
-    }
-    for (int i=1;i<=n;i++)
-        cout<<i<<' '<<sum[0][i]<<' '<<sum[1][i]<<endl;
-    for (int i=1;i<=n;i++)  {
-        nowoddsum=sum[1][i]+sum[n&1][n]-sum[i&1][i];
-        nowevensum=sum[0][i]+sum[n&1][n]-sum[i&1][i];
-        cout<<i<<" nowoddsum: "<<nowoddsum<<" nowevensum: "<<nowevensum<<endl;
-        if (nowoddsum==nowevensum)  ans++;
+        else if (i==1)
+            b=ltor[0];
+        a+=rtol[i+1];
+        b+=rtol[i+2];
+        ans+=(a==b);
     }
     cout<<ans<<endl;
     return 0;
