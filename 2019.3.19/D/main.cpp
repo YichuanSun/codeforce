89095eb67bbf7de7a8c44d30d4c8782794f634cf
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-03-20 00:10:36 +0800

    思路有了，写起来还是比较快的

--- 2019.3.19/D/main.cpp
+++ 2019.3.19/D/main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define N 150005
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+char st1[N],st2[N],t1[N],t2[N];
+int main()  {
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++)   {cin>>st1[i];t1[i]=st1[i];}
+    for (int i=0;i<n;i++)   {cin>>st2[i];t2[i]=st2[i];}
+    for (int i=0;i<n;i++)   cout<<st1[i];
+    cout<<endl;
+    for (int i=0;i<n;i++)   cout<<st2[i];
+    cout<<endl;
     return 0;
 }
