a2703c1a1350e8afb10b025b63400430a71dc081
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-03-23 00:41:40 +0800

    。

--- 2019.3.22/B/main.cpp
+++ 2019.3.22/B/main.cpp
@@ -1,36 +1,27 @@
 #include <bits/stdc++.h>
-#define N 105
 using namespace std;
-string rmv(string ts,char c,int tj);
+
 int main()  {
-    int n,tn;
+    int n,m;
     cin>>n;
-    for (int i=0;i<n;i++)   {
+    while (n--) {
         string s;
-        bool sp[N];
-        fill(sp,sp+N,false);
-        cin>>tn;
+        cin>>m;
         cin>>s;
-        for (int j=0;j<(int)s.size();j++)   {
-            if (s[j]=='>')  {
-                //string ts(s,0,j+1);
-                for (int k=j+1;k<tn;k++)    sp[k]=1;
-                //s=ts;
-                //cout<<ts<<endl;
+        int a1=100000,a2=100000;
+        for (int i=0;i<m;i++)
+            if (s[i]=='>')  {
+                a1=i;
+                break;
             }
-            else {
-                //string ts(s,j);
-                for (int k=0;k<j;k++)   sp[k]=1;
-                //s=ts;
-                //cout<<ts<<endl;
+        for (int i=m-1;i>=0;i--)    {
+            if (s[i]=='<')  {
+                a2=m-1-i;
+                break;
             }
         }
-        string ts="";
-        for (int j=0;j<tn;j++)
-            if (sp[j]==0)
-                ts+=s[j];
-        cout<<s<<endl;
-        cout<<ts.size()-1<<endl;
+        int ans=min(a1,a2);
+        cout<<ans<<endl;
     }
     return 0;
 }
