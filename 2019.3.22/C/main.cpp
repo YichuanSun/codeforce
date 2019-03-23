b1677d7fcda7fdc7a709e0ac98e873b704b82a79
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-03-23 16:35:27 +0800

    有点意思

--- 2019.3.22/C/main.cpp
+++ 2019.3.22/C/main.cpp
@@ -18,6 +18,7 @@ inline void add(ll u){
 	sum+=u;
 	if(pq.size()>m){
 		sum-=pq.top();
+		cout<<"now pop:"<<pq.top()<<endl;
 		pq.pop();
 	}
 }
@@ -28,6 +29,7 @@ int main(){
 	for(i=1;i<=n;i++)
 		scanf("%lld%lld",&num[i].se,&num[i].fi);
 	sort(num+1,num+n+1);
+	cout<<"first=beauty second=time"<<endl;
 	for (int i=1;i<=n;i++)
         cout<<num[i].fi<<' '<<num[i].se<<endl;
 	for(i=n;i>=1;i--){
@@ -36,48 +38,3 @@ int main(){
 	}
 	cout<<ans;
 }
-
-//#include<bits/stdc++.h>
-//#define MAXN 300005
-//#define INF 1000000000
-//#define MOD 1000000007
-//#define F first
-//#define S second
-//using namespace std;
-//typedef long long ll;
-//typedef pair<int,int> P;
-//int n,k;
-//priority_queue <int,vector<int>,greater<int> > pque;
-//P a[MAXN];
-//int main()
-//{
-//    scanf("%d%d",&n,&k);
-//    for(int i=1;i<=n;i++)
-//        scanf("%d%d",&a[i].S,&a[i].F);
-//    sort(a+1,a+n+1,greater<P>());
-//    for (int i=1;i<=n;i++)
-//        cout<<a[i].F<<' '<<a[i].S<<endl;
-//    ll sum=0,ans=0;
-//    for(int i=1;i<=k;i++)
-//    {
-//        pque.push(a[i].S);
-//        sum+=a[i].S;
-//        ans=max(ans,sum*a[i].F);
-//        cout<<"now round="<<i<<' '<<ans<<endl;
-//    }
-//    ans=max(ans,sum*a[k].F);
-//    for(int i=k+1;i<=n;i++)
-//    {
-//        int p=pque.top();
-//        if(a[i].S<=p) continue;
-//        else
-//        {
-//            pque.pop();
-//            pque.push(a[i].S);
-//            sum-=p; sum+=a[i].S;
-//            ans=max(ans,sum*a[i].F);
-//        }
-//    }
-//    cout<<ans<<endl;
-//    return 0;
-//}
