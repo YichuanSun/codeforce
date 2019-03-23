b8dc4c86bb71723ab769895e5fe66ba19de4d5d8
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-03-23 17:47:48 +0800

    太可惜了
    太可惜了，赛场上先是A题脑子进水了，调了半小时，再是B题理解错了，很长时间才弄过来，D题最后十分钟才发现，六七行代码搞完了，D才是全场最水。C题看了一会以为是DP，直接跳过了，还是太年轻了啊。

--- 2019.3.22/C/main.cpp
+++ 2019.3.22/C/main.cpp
@@ -1,40 +1,76 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define db double
-#define P pair<ll,ll>
-#define mp make_pair
-#define fi first
-#define se second
-#define N 1001000
-#define M
+#include <bits/stdc++.h>
+#define N 300010
 using namespace std;
-
-ll n,m,sum,ans;
-P num[N];
-priority_queue<ll,vector<ll>,greater<ll> >pq;
-
-inline void add(ll u){
-	pq.push(u);
-	sum+=u;
-	if(pq.size()>m){
-		sum-=pq.top();
-		cout<<"now pop:"<<pq.top()<<endl;
-		pq.pop();
-	}
+typedef pair<int,int> p;
+typedef long long ll;
+p song[N];
+ll n,k,sum,ans;
+void attemptAdd(int c);
+priority_queue<int,vector<int>,greater<int> > q;
+int main()  {
+    cin>>n>>k;
+    for (int i=0;i<n;i++)
+        cin>>song[i].second>>song[i].first;
+    sort(song,song+n);
+    for (int i=n-1;i>=0;i--)    {
+        attemptAdd(song[i].second);
+        ans=max(ans,sum*song[i].first);
+    }
+    cout<<ans<<endl;
+    return 0;
 }
 
-int main(){
-	ll i,j;
-	cin>>n>>m;
-	for(i=1;i<=n;i++)
-		scanf("%lld%lld",&num[i].se,&num[i].fi);
-	sort(num+1,num+n+1);
-	cout<<"first=beauty second=time"<<endl;
-	for (int i=1;i<=n;i++)
-        cout<<num[i].fi<<' '<<num[i].se<<endl;
-	for(i=n;i>=1;i--){
-		add(num[i].se);
-		ans=max(ans,sum*num[i].fi);
-	}
-	cout<<ans;
+void attemptAdd(int c)   {
+    q.push(c);
+    sum+=c;
+    if ((int)q.size()>k)    {
+        sum-=q.top();
+        q.pop();
+    }
 }
+
+
+
+
+
+
+//#include<bits/stdc++.h>
+//#define ll long long
+//#define db double
+//#define P pair<ll,ll>
+//#define mp make_pair
+//#define fi first
+//#define se second
+//#define N 1001000
+//#define M
+//using namespace std;
+//
+//ll n,m,sum,ans;
+//P num[N];
+//priority_queue<ll,vector<ll>,greater<ll> >pq;
+//
+//inline void add(ll u){
+//	pq.push(u);
+//	sum+=u;
+//	if(pq.size()>m){
+//		sum-=pq.top();
+//		cout<<"now pop:"<<pq.top()<<endl;
+//		pq.pop();
+//	}
+//}
+//
+//int main(){
+//	ll i,j;
+//	cin>>n>>m;
+//	for(i=1;i<=n;i++)
+//		scanf("%lld%lld",&num[i].se,&num[i].fi);
+//	sort(num+1,num+n+1);
+//	cout<<"first=beauty second=time"<<endl;
+//	for (int i=1;i<=n;i++)
+//        cout<<num[i].fi<<' '<<num[i].se<<endl;
+//	for(i=n;i>=1;i--){
+//		add(num[i].se);
+//		ans=max(ans,sum*num[i].fi);
+//	}
+//	cout<<ans;
+//}
